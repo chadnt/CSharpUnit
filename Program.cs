edd1b9bb74d1211789b2f3c095e99f70c0339aa4
Chad Hodnett <chad.hodnett@nucor.com>
2019-05-17 15:02:26 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
